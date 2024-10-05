898b19dc1a00b54e9c969b94ee794164ab922e9a
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-10-05 11:27:46 +0530

    all features added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import CardsDetails from './components/CardsDetails';
+import Cards from './components/Cards';
+import {Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <p>hello</p>
+  <>
+   <Header />
+   <Routes>
+     <Route path='/' element={<Cards />} />
+     <Route path='/cart/:id' element={<CardsDetails />} />
+   </Routes>
+  </>
   );
 }
 
